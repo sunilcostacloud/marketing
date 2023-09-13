6c855a4a7c5ef98f1a670b206c8aca7077815b37
sunilcostacloud <sunil@costacloud.com>
2023-09-13 11:26:58 +0530

    url changed

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = (_, argv) => ({
     publicPath:
       argv.mode === "development"
         ? "http://localhost:8081/"
-        : "https://marketing-microfrontend.apps.ocp4.pacosta.com/eoffice/",
+        : "http://marketing-microfrontend.apps.ocp4.pacosta.com/eoffice/",
   },
 
   resolve: {
